33b1fd18d49e7dda7081f120febd37fb752ed87b
Iqbaldhia <iqbaldhiaa@gmail.com>
2022-06-13 12:31:47 +0700

    feat: fetching fn

--- ios-randomize-photo/ViewController.swift
+++ ios-randomize-photo/ViewController.swift
@@ -2,16 +2,61 @@
 //  ViewController.swift
 //  ios-randomize-photo
 //
-//  Created by Aldi on 13/06/22.
+//  Created by xbaldiq on 13/06/22.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.backgroundColor = .white
+        return imageView
+    }()
+    
+    private let button: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .white
+        button.setTitle("Random Photo", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBlue
+        view.addSubview(imageView)
+        imageView.frame = CGRect(x: 0, y: 0, width: 300, height: 300)
+        imageView.center = view.center
+        
+        view.addSubview(button)
+        
+        getRandomPhoto()
+        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        button.frame = CGRect(x: 30,
+                              y: view.frame.size.height-150-view.safeAreaInsets.bottom,
+                              width: view.frame.size.width-60,
+                              height: 55)
+    }
+    
+    @objc func didTapButton() {
+        getRandomPhoto()
+    }
+    
+    func getRandomPhoto() {
+        let urlString = "https://source.unsplash.com/random/600x600"
+        let url = URL(string: urlString)!
+        guard let data = try? Data(contentsOf: url) else {
+            return
+        }
+        imageView.image = UIImage(data: data)
     }
 
 
